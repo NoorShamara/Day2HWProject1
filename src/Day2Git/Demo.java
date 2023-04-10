6a0ba49a77579502fa465b772d1a5e0b0f0b0cb4
noors <noors@Noor>
2023-04-10 13:44:07 -0400

    Add()

---
+++ src/Day2Git/Demo.java
@@ -0,0 +1,12 @@
+package Day2Git;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	System.out.println("hello world..");
+
+	}
+public static void add() {
+	System.out.println(1+2);
+}
+}
